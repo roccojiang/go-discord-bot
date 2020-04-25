2841f3769fa899f8d66920d6abb9c07c03dc39f3
rj14ng <rocco.jiang5@gmail.com>
2020-04-25 22:03:41 +0800

    Add help command and refactor code

--- go.mod
+++ go.mod
@@ -6,7 +6,9 @@ require (
 	github.com/bas24/googletranslatefree v0.0.0-20170719053803-07873a6de396
 	github.com/bwmarrin/discordgo v0.20.3
 	github.com/joho/godotenv v1.3.0
+	github.com/mattn/go-shellwords v1.0.10
 	github.com/nishanths/go-xkcd v0.0.0-20160531092901-9497d909de39
 	github.com/robertkrimen/otto v0.0.0-20191219234010-c382bd3c16ff // indirect
+	github.com/stretchr/testify v1.5.1 // indirect
 	gopkg.in/sourcemap.v1 v1.0.5 // indirect
 )
--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 
 	gt "github.com/bas24/googletranslatefree"
 	dg "github.com/bwmarrin/discordgo"
+	parse "github.com/mattn/go-shellwords"
 	xkcd "github.com/nishanths/go-xkcd"
 )
 
@@ -18,35 +18,35 @@ var (
 )
 
 func main() {
-	// Create a new Discord session using the provided bot token.
+	// Create a new Discord session using the provided bot token
 	dg, err := dg.New("Bot " + Token)
 	if err != nil {
-		fmt.Println("error creating Discord session,", err)
+		fmt.Println("Error creating Discord session,", err)
 		return
 	}
 
-	// Register the messageCreate func as a callback for MessageCreate events.
+	// Register the messageCreate func as a callback for MessageCreate events
 	dg.AddHandler(messageCreate)
 
-	// Open a websocket connection to Discord and begin listening.
+	// Open a websocket connection to Discord and begin listening
 	err = dg.Open()
 	if err != nil {
-		fmt.Println("error opening connection,", err)
+		fmt.Println("Error opening connection,", err)
 		return
 	}
 
-	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	// Wait here until CTRL-C or other term signal is received
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
-	// Cleanly close down the Discord session.
+	// Cleanly close down the Discord session
 	dg.Close()
 }
 
 // This function will be called (due to AddHandler above) every time a new
-// message is created on any channel that the autenticated bot has access to.
+// message is created on any channel that the authenticated bot has access to
 func messageCreate(s *dg.Session, m *dg.MessageCreate) {
 
 	// Ignore all messages created by the bot itself
@@ -55,41 +55,80 @@ func messageCreate(s *dg.Session, m *dg.MessageCreate) {
 		return
 	}
 
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	// Parse commands
+	input, err := parse.Parse(m.Content)
+	if err != nil {
+		fmt.Println("Error parsing input,", err)
+		return
 	}
 
-	// If the message is "pong" reply with "Ping!"
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "Ping!")
-	}
+	switch input[0] {
+	// Ping
+	case "!ping":
+		s.ChannelMessageSend(m.ChannelID, "Pong")
 
-	// Translate Andrei's weird German text
-	if strings.HasPrefix(m.Content, "!whatisandreisaying") {
-		en := strings.TrimPrefix(m.Content, "!whatisandreisaying ")
-		de, _ := gt.Translate(en, "de", "en")
-		s.ChannelMessageSend(m.ChannelID, de)
-	}
+	// Google Translate
+	// Mostly for Andrei's weird German text
+	case "!translate":
+		if len(input) != 4 {
+			s.ChannelMessageSend(m.ChannelID, "Invalid input for !translate. Check !help for an overview.")
+			return
+		}
 
-	if strings.HasPrefix(m.Content, "!xkcd") {
+		text := input[1]
+		in_lang := input[2]
+		out_lang := input[3]
+
+		translated, _ := gt.Translate(text, in_lang, out_lang)
+		s.ChannelMessageSend(m.ChannelID, translated)
+
+	// Random xkcd comic
+	case "!xkcd":
+		// Create xkcd client and get random comic
 		xkcdClient := xkcd.NewClient()
 		comic, _ := xkcdClient.Random()
 
-		img := &dg.MessageEmbedImage{URL: comic.ImageURL}
-		author := &dg.MessageEmbedAuthor{
-			URL:     "https://xkcd.com",
-			Name:    "xkcd",
-			IconURL: "https://is2-ssl.mzstatic.com/image/thumb/Purple123/v4/b5/f6/a2/b5f6a20c-5e4e-fb72-2592-2841784bc48c/AppIcon-0-0-1x_U007emarketing-0-0-0-5-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.jpeg/320x0w.jpg",
+		// Create embed
+		embed := &dg.MessageEmbed{
+			Author: &dg.MessageEmbedAuthor{
+				Name:    "xkcd",
+				URL:     "https://xkcd.com",
+				IconURL: "https://is2-ssl.mzstatic.com/image/thumb/Purple123/v4/b5/f6/a2/b5f6a20c-5e4e-fb72-2592-2841784bc48c/AppIcon-0-0-1x_U007emarketing-0-0-0-5-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.jpeg/320x0w.jpg",
+			},
+			Color: 0x96aac8,
+			URL:   fmt.Sprintf("https://xkcd.com/%d", comic.Number),
+			Title: fmt.Sprintf("#%d - %s", comic.Number, comic.Title),
+			Image: &dg.MessageEmbedImage{URL: comic.ImageURL},
 		}
+		s.ChannelMessageSendEmbed(m.ChannelID, embed)
 
-		msg := &dg.MessageEmbed{
-			Author: author,
-			Color:  0x96aac8,
-			URL:    fmt.Sprintf("https://xkcd.com/%d", comic.Number),
-			Title:  fmt.Sprintf("#%d - %s", comic.Number, comic.Title),
-			Image:  img,
+	// Help command
+	case "!help":
+		embed := &dg.MessageEmbed{
+			Author: &dg.MessageEmbedAuthor{
+				Name:    s.State.User.Username,
+				IconURL: s.State.User.AvatarURL(""),
+			},
+			Title: "Commands",
+			Fields: []*dg.MessageEmbedField{
+				&dg.MessageEmbedField{
+					Name:  "!help",
+					Value: "Invokes this help box... but it's obvious you know that already.",
+				},
+				&dg.MessageEmbedField{
+					Name:  "!ping",
+					Value: "Check if the bot is alive.",
+				},
+				&dg.MessageEmbedField{
+					Name:  "!translate [text] [input lang] [output lang]",
+					Value: "Uses Google Translate. Keep text in quotations, and use two-letter language codes.",
+				},
+				&dg.MessageEmbedField{
+					Name:  "!xkcd",
+					Value: "Shows a random xkcd comic.",
+				},
+			},
 		}
-		s.ChannelMessageSendEmbed(m.ChannelID, msg)
+		s.ChannelMessageSendEmbed(m.ChannelID, embed)
 	}
 }
