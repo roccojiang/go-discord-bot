a1f5520eb46b9e259ab7d82822ab85855787ee9d
rj14ng <rocco.jiang5@gmail.com>
2020-04-26 16:03:44 +0800

    Add tweet command and refactor

--- main.go
+++ main.go
@@ -2,24 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
 	gt "github.com/bas24/googletranslatefree"
-	dg "github.com/bwmarrin/discordgo"
+	d "github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 	parse "github.com/mattn/go-shellwords"
 	xkcd "github.com/nishanths/go-xkcd"
 )
 
-// Initialise token variable
 var (
-	Token = os.Getenv("BOT_SECRET")
+	botToken       string
+	twitterAccount string
 )
+
+// Load .env file and token variable
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	botToken = os.Getenv("BOT_SECRET")
+	twitterAccount = os.Getenv("TWITTER_ACCOUNT")
+}
 
 func main() {
 	// Create a new Discord session using the provided bot token
-	dg, err := dg.New("Bot " + Token)
+	dg, err := d.New("Bot " + botToken)
 	if err != nil {
 		fmt.Println("Error creating Discord session,", err)
 		return
@@ -47,10 +60,9 @@ func main() {
 
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the authenticated bot has access to
-func messageCreate(s *dg.Session, m *dg.MessageCreate) {
+func messageCreate(s *d.Session, m *d.MessageCreate) {
 
 	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
@@ -62,73 +74,85 @@ func messageCreate(s *dg.Session, m *dg.MessageCreate) {
 		return
 	}
 
-	switch input[0] {
-	// Ping
-	case "!ping":
-		s.ChannelMessageSend(m.ChannelID, "Pong")
+	if len(input) > 0 {
+		command := input[0]
+		switch command {
+		// Ping
+		case "!ping":
+			s.ChannelMessageSend(m.ChannelID, "Pong")
 
-	// Google Translate
-	// Mostly for Andrei's weird German text
-	case "!translate":
-		if len(input) != 4 {
-			s.ChannelMessageSend(m.ChannelID, "Invalid input for !translate. Check !help for an overview.")
-			return
+		// Google Translate
+		// Mostly for Andrei's weird German text
+		case "!translate":
+			if len(input) != 4 {
+				s.ChannelMessageSend(m.ChannelID, "Invalid input for !translate. Check !help for an overview.")
+				return
+			}
+
+			text := input[1]
+			in_lang := input[2]
+			out_lang := input[3]
+
+			translated, _ := gt.Translate(text, in_lang, out_lang)
+			s.ChannelMessageSend(m.ChannelID, translated)
+
+		// Random xkcd comic
+		case "!xkcd":
+			// Create xkcd client and get random comic
+			xkcdClient := xkcd.NewClient()
+			comic, _ := xkcdClient.Random()
+
+			// Create embed
+			embed := &d.MessageEmbed{
+				Author: &d.MessageEmbedAuthor{
+					Name:    "xkcd",
+					URL:     "https://xkcd.com",
+					IconURL: "https://is2-ssl.mzstatic.com/image/thumb/Purple123/v4/b5/f6/a2/b5f6a20c-5e4e-fb72-2592-2841784bc48c/AppIcon-0-0-1x_U007emarketing-0-0-0-5-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.jpeg/320x0w.jpg",
+				},
+				Color: 0x96aac8,
+				URL:   fmt.Sprintf("https://xkcd.com/%d", comic.Number),
+				Title: fmt.Sprintf("#%d - %s", comic.Number, comic.Title),
+				Image: &d.MessageEmbedImage{URL: comic.ImageURL},
+			}
+			s.ChannelMessageSendEmbed(m.ChannelID, embed)
+
+		// Tweet command
+		case "!tweet":
+			embed := createTweetEmbed(twitterAccount)
+			s.ChannelMessageSendEmbed(m.ChannelID, embed)
+
+		// Help command
+		case "!help":
+			embed := &d.MessageEmbed{
+				Author: &d.MessageEmbedAuthor{
+					Name:    s.State.User.Username,
+					IconURL: s.State.User.AvatarURL(""),
+				},
+				Title: "Commands",
+				Fields: []*d.MessageEmbedField{
+					&d.MessageEmbedField{
+						Name:  "!help",
+						Value: "Invokes this help box... but it's obvious you know that already.",
+					},
+					&d.MessageEmbedField{
+						Name:  "!ping",
+						Value: "Check if the bot is alive.",
+					},
+					&d.MessageEmbedField{
+						Name:  "!translate [text] [input lang] [output lang]",
+						Value: "Uses Google Translate. Keep text in quotations, and use two-letter language codes.",
+					},
+					&d.MessageEmbedField{
+						Name:  "!xkcd",
+						Value: "Shows a random xkcd comic.",
+					},
+					&d.MessageEmbedField{
+						Name:  "!tweet",
+						Value: "Shows a random tweet from our favourite news source.",
+					},
+				},
+			}
+			s.ChannelMessageSendEmbed(m.ChannelID, embed)
 		}
-
-		text := input[1]
-		in_lang := input[2]
-		out_lang := input[3]
-
-		translated, _ := gt.Translate(text, in_lang, out_lang)
-		s.ChannelMessageSend(m.ChannelID, translated)
-
-	// Random xkcd comic
-	case "!xkcd":
-		// Create xkcd client and get random comic
-		xkcdClient := xkcd.NewClient()
-		comic, _ := xkcdClient.Random()
-
-		// Create embed
-		embed := &dg.MessageEmbed{
-			Author: &dg.MessageEmbedAuthor{
-				Name:    "xkcd",
-				URL:     "https://xkcd.com",
-				IconURL: "https://is2-ssl.mzstatic.com/image/thumb/Purple123/v4/b5/f6/a2/b5f6a20c-5e4e-fb72-2592-2841784bc48c/AppIcon-0-0-1x_U007emarketing-0-0-0-5-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.jpeg/320x0w.jpg",
-			},
-			Color: 0x96aac8,
-			URL:   fmt.Sprintf("https://xkcd.com/%d", comic.Number),
-			Title: fmt.Sprintf("#%d - %s", comic.Number, comic.Title),
-			Image: &dg.MessageEmbedImage{URL: comic.ImageURL},
-		}
-		s.ChannelMessageSendEmbed(m.ChannelID, embed)
-
-	// Help command
-	case "!help":
-		embed := &dg.MessageEmbed{
-			Author: &dg.MessageEmbedAuthor{
-				Name:    s.State.User.Username,
-				IconURL: s.State.User.AvatarURL(""),
-			},
-			Title: "Commands",
-			Fields: []*dg.MessageEmbedField{
-				&dg.MessageEmbedField{
-					Name:  "!help",
-					Value: "Invokes this help box... but it's obvious you know that already.",
-				},
-				&dg.MessageEmbedField{
-					Name:  "!ping",
-					Value: "Check if the bot is alive.",
-				},
-				&dg.MessageEmbedField{
-					Name:  "!translate [text] [input lang] [output lang]",
-					Value: "Uses Google Translate. Keep text in quotations, and use two-letter language codes.",
-				},
-				&dg.MessageEmbedField{
-					Name:  "!xkcd",
-					Value: "Shows a random xkcd comic.",
-				},
-			},
-		}
-		s.ChannelMessageSendEmbed(m.ChannelID, embed)
 	}
 }
