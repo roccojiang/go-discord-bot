6ccb58445e0a97bb87f28b1a288eb4adc1e4cd62
rj14ng <rocco.jiang5@gmail.com>
2020-04-26 16:44:11 +0800

    Separate help command to separate file

---
+++ help.go
@@ -0,0 +1,38 @@
+package main
+
+import d "github.com/bwmarrin/discordgo"
+
+// createComicEmbed creates a pointer to a Discord MessageEmbed object with a help box
+func createHelpEmbed(s *d.Session) *d.MessageEmbed {
+	embed := &d.MessageEmbed{
+		Author: &d.MessageEmbedAuthor{
+			Name:    s.State.User.Username,
+			IconURL: s.State.User.AvatarURL(""),
+		},
+		Title: "Commands",
+		Fields: []*d.MessageEmbedField{
+			&d.MessageEmbedField{
+				Name:  "!help",
+				Value: "Invokes this help box... but it's obvious you know that already.",
+			},
+			&d.MessageEmbedField{
+				Name:  "!ping",
+				Value: "Check if the bot is alive.",
+			},
+			&d.MessageEmbedField{
+				Name:  "!tweet",
+				Value: "Shows a random tweet from our favourite news source.",
+			},
+			&d.MessageEmbedField{
+				Name:  "!translate [text] [input lang] [output lang]",
+				Value: "Uses Google Translate. Keep text in quotations, and use two-letter language codes.",
+			},
+			&d.MessageEmbedField{
+				Name:  "!xkcd",
+				Value: "Shows a random xkcd comic.",
+			},
+		},
+	}
+
+	return embed
+}
